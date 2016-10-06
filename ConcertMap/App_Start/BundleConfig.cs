b451204db443f49bc96b30ad8d4930dd535d2f86
resitamas <resitomi@gmail.com>
2016-10-06 23:39:47 +0200

    jvectormap berakása projektbe

--- ConcertMap/App_Start/BundleConfig.cs
+++ ConcertMap/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace ConcertMap
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+
+            bundles.Add(new ScriptBundle("~/bundles/jvectormap").Include(
+                        "~/Scripts/jquery-jvectormap.js",
+                        "~/Scripts/jquery-mousewheel.js").IncludeDirectory("~/Scripts/jVectorMaps/world-ISO-A2","*.js",true));
+
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,7 +31,9 @@ namespace ConcertMap
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery-jvectormap.css"));
+
         }
     }
 }
