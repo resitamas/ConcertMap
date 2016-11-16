c825fc05a3c4ae0723e506fa36a192a0897488c9
Eszti <eszter.meglecz@gmail.com>
2016-11-16 20:50:00 +0100

    Cookie-k

--- ConcertMap/Controllers/HomeController.cs
+++ ConcertMap/Controllers/HomeController.cs
@@ -26,11 +26,43 @@ namespace ConcertMap.Controllers
 
             if (artist == null)
             {
-                model.fromDate = Convert.ToDateTime("1900-01-01 00:00:01");
-                model.toDate = Convert.ToDateTime("2100-01-01 00:00:01");
-                model.isUpcoming = true;
-                model.isPast = true;
+                HttpCookie cookie_request = Request.Cookies["search"];
 
+                if (cookie_request != null)
+                {
+                    model.ArtistName = cookie_request.Values["artistName"];
+                    model.isPast = Convert.ToBoolean(cookie_request.Values["isPast"]);
+                    model.isUpcoming = Convert.ToBoolean(cookie_request.Values["isUpcoming"]);
+
+                    if (model.isPast && model.isUpcoming)
+                    {
+                        model.fromDate = Convert.ToDateTime("1900-01-01 00:00:01");
+                        model.toDate = Convert.ToDateTime("2100-01-01 00:00:01");
+                    }
+                    if (model.isPast && !model.isUpcoming)
+                    {
+                        model.fromDate = Convert.ToDateTime("1900-01-01 00:00:01");
+                        model.toDate = DateTime.Now;
+                    }
+                    if (!model.isPast && model.isUpcoming)
+                    {
+                        model.fromDate = DateTime.Now;
+                        model.toDate = Convert.ToDateTime("2100-01-01 00:00:01");
+                    }
+                    if (!model.isPast && !model.isUpcoming)
+                    {
+                        model.fromDate = DateTime.Now;
+                        model.toDate = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    model.fromDate = Convert.ToDateTime("1900-01-01 00:00:01");
+                    model.toDate = Convert.ToDateTime("2100-01-01 00:00:01");
+                    model.isUpcoming = true;
+                    model.isPast = true;
+                }
+                
                 return View(model);
             }
 
@@ -60,6 +92,15 @@ namespace ConcertMap.Controllers
             model.ArtistName = artist;
             model.isUpcoming = upcoming;
             model.isPast = past;
+
+
+            HttpCookie cookie = new HttpCookie("search");
+            cookie.Values["artistName"] = model.ArtistName;
+            cookie.Values["isPast"] = model.isPast.ToString();
+            cookie.Values["isUpcoming"] = model.isUpcoming.ToString();
+            cookie.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Add(cookie);
+
 
             return View(model);
         }
